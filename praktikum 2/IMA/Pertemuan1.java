7dc51746a59eb86812552d96a1738e217c0c4391
SitiShafira <sitishafira1801@gmail.com>
2020-09-13 11:12:47 +0700

    Pertemuan1.java

---
+++ praktikum 2/IMA/Pertemuan1.java
@@ -0,0 +1,8 @@
+    public class Pertemuan1 {
+	    public static void main(String[] args) {
+			System.out.println(" Nama: Siti Shafira");
+			System.out.println("Nim: 6706204040");
+			System.out.println("Kelas: D3IF 44-04");
+         }
+	 }
+	
